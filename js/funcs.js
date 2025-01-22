c27a78faedfc817e34f8460bc0624b573c809659
Josef Mariánek <marianek@gym-tisnov.cz>
2025-01-22 09:19:16 +0100

    Zpravy pres REST GET sluzbu + DOM operace

--- js/funcs.js
+++ js/funcs.js
@@ -5,14 +5,27 @@
  */
 function show_posts(room_id)
 {
-    let div = document.getElementById("id_posts");
+    let allPostsDiv = document.getElementById("id_posts");
+    allPostsDiv.innerHTML = "";
+
     // volani sluzby 
     // http://localhost/4r/chat/service/get_posts.php?rooms_id=
     $.get("http://localhost/4r/chat/service/get_posts.php?rooms_id="
-           + room_id, function(data) {
+           + room_id, function(data)
+    {
+        // zakomentovano - jQuery provedl parsovani interne
+        //let posts = JSON.parse(data);
+
         console.log(data);
-        // TODO json encode + DOM operace
-        div.innerHTML = data;
+        
+        for (let post of data) {
+            console.log(post.id + ' ' + post.msg);
+            let postDiv = document.createElement("div");
+            postDiv.className = "post";
+            postDiv.innerHTML = post.msg;
+            allPostsDiv.appendChild(postDiv);
+        }
+
     });
 
      
