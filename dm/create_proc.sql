8460217276768b5eb5a82638e2f789ad89425386
Josef Mariánek <marianek@gym-tisnov.cz>
2025-03-12 07:45:13 +0100

    ADD_USER() s out par.

--- dm/create_proc.sql
+++ dm/create_proc.sql
@@ -1,16 +1,17 @@
+-- procedury pro chat
+-- verze 2025-03-12
+
 USE `chat`;
 DROP procedure IF EXISTS `add_user`;
 
-USE `chat`;
-DROP procedure IF EXISTS `chat`.`add_user`;
-;
 
 DELIMITER $$
 USE `chat`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `add_user`(
    IN p_login varchar(20),
    IN p_passwd varchar(32),
-   IN p_role varchar(10))
+   IN p_role varchar(10),
+   out p_id int) -- vystupni parametr
 BEGIN
     -- TODO - kontrola, jestli p_role obsahuje
     -- jedno z admin/user
@@ -20,8 +21,17 @@ BEGIN
     end if;
     insert into users(login, passwd, role)
     values(p_login, p_passwd, p_role);
+    
+    -- id posledniho vlozeneho zaznamu do out par. p_id
+    select LAST_INSERT_ID() into p_id;
 END$$
 
+
 DELIMITER ;
-;
+
+call add_user('user8', '12345', 'user', @user_id);
+
+select @user_id;
+-- 16
+
 
